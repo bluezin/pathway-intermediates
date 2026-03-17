04f3d4c2ac0470efd31b15fbbf9165125ac848c2
yadira <yadiraco304@gmail.com>
2026-03-17 18:25:49 -0500

    update: change image and format

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,7 +8,7 @@ const About = () => {
       <Head title="About" description="About" />
 
       <Header
-        banner="/images/banners/banner-about.webp"
+        banner="/images/banners/banner-about.png"
         title="<p style='font-size: 31px; padding-right: 160px'>We are</p>Pathway"
       />
 
