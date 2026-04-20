7a2bf62026172275707414e2813de752b8772a94
yadira <yadiraco304@gmail.com>
2026-04-20 16:44:10 -0500

    refactor: remove code unnecessary

--- src/components/templates/home/index.tsx
+++ src/components/templates/home/index.tsx
@@ -1,11 +1,9 @@
 import Relations from "./components/relations";
 import Notices from "./components/notices";
-import CompanionAnimal from "./components/companion-animal";
 
 const HomeTempalte = () => {
   return (
     <section>
-      <CompanionAnimal />
       <Notices />
       <Relations />
     </section>
