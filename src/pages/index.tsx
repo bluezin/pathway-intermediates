624c66344bc9f9970fec0d763d2fecd165b8376e
yadira <yadiraco304@gmail.com>
2026-04-28 18:48:56 -0500

    add title and description for seo of index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import Seo from "@/components/atoms/seo";
 import HomeMainTemplate from "@/components/templates/home-main";
 
 export default function App() {
   return (
     <>
+      <Seo title="Pathway US vs Biomatrix" description="" />
       <HomeMainTemplate />
     </>
   );
